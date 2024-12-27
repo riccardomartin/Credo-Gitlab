1a1c2edd64a33d321f2f23a05ddaa3da19e87128
rmartin89 <martin.riccardo@gmail.com>
2024-12-27 16:27:00 +0000

    Add base files

---
+++ mix.exs
@@ -0,0 +1,27 @@
+defmodule CredoGitlab.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :credo_gitlab,
+      description: "Credo plugin for exporting GitLab Code Quality reports",
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:credo, ">= 0.0.0"},
+      {:jason, "~> 1.2"}
+    ]
+  end
+end
