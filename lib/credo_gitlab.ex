bec9705f65b609c3ba565da742c751330676d2c4
rmartin89 <martin.riccardo@gmail.com>
2024-12-27 16:27:13 +0000

    Add base files

---
+++ lib/credo_gitlab.ex
@@ -0,0 +1,12 @@
+defmodule CredoGitlab do
+  @moduledoc """
+  Documentation for `CredoGitlab`.
+  """
+  import Credo.Plugin
+
+  alias Credo.Execution
+
+  def init(exec) do
+    exec
+  end
+end
---
+++ test/credo_gitlab_test.exs
@@ -0,0 +1,8 @@
+defmodule CredoGitlabTest do
+  use ExUnit.Case
+  doctest CredoGitlab
+
+  test "greets the world" do
+    assert CredoGitlab.hello() == :world
+  end
+end
