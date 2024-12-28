38dc233c66fcd0ac7959f788369219559eab2afd
rmartin89 <martin.riccardo@gmail.com>
2024-12-28 12:03:08 +0000

    Update command name

--- lib/credo_gitlab.ex
+++ lib/credo_gitlab.ex
@@ -4,7 +4,7 @@ defmodule CredoGitlab do
   """
   import Credo.Plugin
 
-  alias CredoGitlab.Commands.GitlabFormatter
+  alias CredoGitlab.Commands.ReportCodequality
 
   @config_file File.read!(".credo.exs")
 
@@ -15,7 +15,7 @@ defmodule CredoGitlab do
   def init(exec) do
     exec
     |> register_default_config(@config_file)
-    |> register_command("report.codequality", GitlabFormatter)
+    |> register_command("report.codequality", ReportCodequality)
     |> register_cli_switch(:file_name, :string, :f)
   end
 end
--- lib/credo_gitlab/commands/report_codequality.ex
+++ lib/credo_gitlab/commands/report_codequality.ex
@@ -1,4 +1,4 @@
-defmodule CredoGitlab.Commands.GitlabFormatter do
+defmodule CredoGitlab.Commands.ReportCodequality do
   @moduledoc """
   Formatter for GitLab Code Quality reports.
   """
