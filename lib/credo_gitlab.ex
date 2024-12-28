44f508ad4232a48ea7d8b7b8fc1e3cf739cc853a
rmartin89 <martin.riccardo@gmail.com>
2024-12-28 14:29:19 +0000

    Remove commented lines

--- lib/credo_gitlab.ex
+++ lib/credo_gitlab.ex
@@ -6,19 +6,11 @@ defmodule CredoGitlab do
 
   alias CredoGitlab.Tasks.ReportCodequality
 
-  # @config_file File.read!(".credo.exs")
-
   @doc """
   Initializes the plugin.
   """
   @spec init(exec :: Credo.Execution.t()) :: Credo.Execution.t()
   def init(exec) do
     append_task(exec, :run_command, ReportCodequality)
-
-    # exec
-    # |> register_default_config(@config_file)
-    # |> register_command("report.codequality", ReportCodequality)
-
-    # |> register_cli_switch(:file_name, :string, :f)
   end
 end
