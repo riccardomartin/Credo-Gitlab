f2735456d4fa0b02086e6f72dd6d61c9e725099e
rmartin89 <martin.riccardo@gmail.com>
2024-12-28 15:27:13 +0000

    Update mix file

--- mix.exs
+++ mix.exs
@@ -1,28 +1,44 @@
 defmodule CredoGitlab.MixProject do
   use Mix.Project
 
+  @source_url "https://gitlab.com/oss5572952/credo-gitlab"
+
   def project do
     [
       app: :credo_gitlab,
-      description: "Credo plugin for exporting GitLab Code Quality reports",
-      version: "0.1.0",
+      description: description(),
+      version: "1.0.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      source_url: @source_url
     ]
   end
 
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   defp deps do
     [
       {:credo, ">= 0.0.0"},
-      {:ex_doc, "~> 0.36.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:jason, "~> 1.2", optional: true}
+    ]
+  end
+
+  defp description do
+    "Credo plugin for exporting GitLab Code Quality reports"
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitLab" => @source_url,
+        "Credo" => "https://hex.pm/packages/credo"
+      }
     ]
   end
 end
